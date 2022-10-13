ddf3f19a2f4d8d1e2cce7d800fad62dfc4ac365d
Kennedy Lopes <kendylopes@gmail.com>
2022-10-13 09:51:41 -0400

    ci: Add publish storybook workflow corrigindo1

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -17,7 +17,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/ignite-lab-ds'
+      config.base = '/ignite-lab-ds/'
     }
     return config
   }
